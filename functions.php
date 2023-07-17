57b923e60d17bba661fe0c704111cb7ba60a01e3
Alejandro Gimeno Martin <alejandro@xanasystem.com>
2023-07-17 10:49:27 +0200

    Fix: Añadida funcion que permite mostrar el nuevo campo en el formulario de checkout

--- functions.php
+++ functions.php
@@ -1,14 +1,24 @@
 <?php
 
 add_filter( 'woocommerce_shipping_calculator_enable_address', '__return_true' );
-add_action('woocommerce_calculated_shipping','codigoverso_add_calculated_shipping_fields');
 
-/**
- * Añadimos a la calculadora el campo de dirección, previamente se ha modificado el shipping-calculator.php para añadirle este campo.
- */
-function codigoverso_add_calculated_shipping_fields() {
-    $address['street']     = isset( $_POST['address'] ) ? wc_clean( wp_unslash( $_POST['address'] ) ) : '';
-    if($address['street'] != '') {
-        WC()->customer->set_shipping_address( $address['street'] );
+// Guardar la dirección en la sesión cuando se actualiza la calculadora de envío
+function codigoverso_update_shipping_address() {
+    if(isset($_POST['calc_shipping_address'])) {
+        $address = sanitize_text_field($_POST['calc_shipping_address']);
+        WC()->session->set('shipping_address_1', $address);
     }
 }
+add_action('woocommerce_calculated_shipping', 'codigoverso_update_shipping_address');
+
+// Añadir la dirección guardada al formulario de pago
+function codigoverso_custom_checkout_fields($fields) {
+    if($address = WC()->session->get('shipping_address_1')) {
+        $fields['shipping']['shipping_address_1']['default'] = $address;
+    }
+    return $fields;
+}
+
+add_action('plugins_loaded', function (){
+    add_filter('woocommerce_checkout_fields', 'custom_checkout_fields',10,1);
+});
